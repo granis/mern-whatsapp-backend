4f989ade4e0a224b6dac4b57c4d1752b41e3d80c
granis <granis@gmail.com>
2021-07-01 11:30:18 +0200

    message sync route

--- server.js
+++ server.js
@@ -20,7 +20,17 @@ mongoose.connect(connection_url, {
 // api end points
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.post("/api/messages/new", (req, res) => {
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
   Messages.create(dbMessage, (err, data) => {

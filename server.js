c2f91044241e665c01033e5c7de7c5372fdc3dab
granis <granis@gmail.com>
2021-07-01 11:04:13 +0200

    add db connection and dotenv

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 // app config
 const app = express();
@@ -7,6 +8,12 @@ const port = process.env.PORT || 9000;
 // middlewares
 
 // db setup
+const connection_url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // api end points
 app.get("/", (req, res) => res.status(200).send("hello world"));
